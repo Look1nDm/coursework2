56c9af92705a9c3ee42cfe43d972497695992530
dimka <dimka411841994@gmail.com>
2022-09-01 15:57:57 +0300

    ДЗ

--- src/main/java/homework/coursework2/EmployeeService.java
+++ src/main/java/homework/coursework2/EmployeeService.java
@@ -3,61 +3,47 @@ package homework.coursework2;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class EmployeeService {
-    private final List<Employee> listEmployee = new ArrayList<>(10);
+    private final Map<String,Employee> listEmployee;
 
+    public EmployeeService() {
+        this.listEmployee = new HashMap<>();
+    }
     public Employee addEmployees(String firstName, String lastName) {
         Employee emp = new Employee(firstName, lastName);
-        try {
-            if (listEmployee.size() == 10) {
-                throw new EmployeeStorageIsFullException("Список сотрудников переполнен!");
-            }
-            if (listEmployee.contains(emp)) {
+            if (listEmployee.containsKey(emp.getFullName())) {
                 throw new EmployeeAlreadyAddedException("Такой сотрудник уже есть в штате!");
             }
-        } catch (EmployeeStorageIsFullException | EmployeeAlreadyAddedException e) {
-            System.out.println(e.getMessage());
-            //тут же не нужно приравнивать emp к null???? ведь когда метод закончит работу ссылка потеряется и ее соберет сборщик мусора????
-        }
-        listEmployee.add(emp);
+        listEmployee.put(emp.getFullName(),emp);
         return emp;
     }
 
     public Employee removeEmployee(String firstName, String lastName) {
         Employee emp = new Employee(firstName, lastName);
-        for (Employee employee : listEmployee) {
-            try {
-                if (employee.equals(emp)) {
-                    listEmployee.remove(employee);
+                if (listEmployee.containsKey(emp.getFullName())) {
+                    listEmployee.remove(emp.getFullName());
                 } else {
                     throw new EmployeeNotFoundException("Сотрудник не найден!");
                 }
-            } catch (EmployeeNotFoundException e) {
-                System.out.println(e.getMessage());
-            }
-        }
         return emp;
     }
 
     public Employee findEmployee(String firstName, String lastName) {
         Employee emp = new Employee(firstName, lastName);
-        try {
-            if (listEmployee.contains(emp)){
+            if (listEmployee.containsKey(emp.getFullName())){
                 return emp;
             } else {
                 throw new EmployeeNotFoundException("Сотудник не найден!");
             }
-        } catch (EmployeeNotFoundException e){
-            System.out.println(e.getMessage());
-        }
-        return emp;
     }
 
     public List<Employee> printEmployees(){
-        return listEmployee;
+        return new ArrayList<>(listEmployee.values());
     }
 }
 
--- src/main/java/homework/coursework2/EmployeeStorageIsFullException.java
+++
@@ -1,7 +0,0 @@
-package homework.coursework2;
-
-public class EmployeeStorageIsFullException extends RuntimeException {
-    public EmployeeStorageIsFullException(String message) {
-        super(message);
-    }
-}
